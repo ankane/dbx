1bb8adf34c9cab77a65351d6f9b528af7cf5733d
Andrew <acekane1@gmail.com>
2018-07-15 01:42:29 -0700

    Clean up unused column [skip ci]

--- tests/testthat/test-postgresql.R
+++ tests/testthat/test-postgresql.R
@@ -5,7 +5,7 @@ skip_on_cran()
 db <- dbxConnect(adapter="rpostgresql", dbname="dbx_test")
 
 dbExecute(db, "DROP TABLE IF EXISTS orders")
-dbExecute(db, "CREATE TABLE orders (id SERIAL PRIMARY KEY, city text, other text)")
+dbExecute(db, "CREATE TABLE orders (id SERIAL PRIMARY KEY, city text)")
 
 dbExecute(db, "DROP TABLE IF EXISTS events")
 dbExecute(db, "CREATE TABLE events (id SERIAL PRIMARY KEY, speed FLOAT, distance DECIMAL, created_on DATE, updated_at TIMESTAMP, deleted_at TIMESTAMPTZ, open_time TIME, close_time TIMETZ, active BOOLEAN, properties JSON, propertiesb JSONB, image BYTEA)")
