210615c1c76c6c79aafc56d00d84f38a62122bcc
Andrew <acekane1@gmail.com>
2018-08-30 08:09:58 +0200

    Set time zone in more visible file [skip ci]

--- tests/testthat/helper-run-tests.R
+++ tests/testthat/helper-run-tests.R
@@ -1,3 +1,6 @@
+# set time zone
+Sys.setenv(TZ="America/Los_Angeles")
+
 runTests <- function(db, redshift=FALSE) {
   orders <- data.frame(id=c(1, 2), city=c("San Francisco", "Boston"), stringsAsFactors=FALSE)
   new_orders <- data.frame(id=c(3, 4), city=c("New York", "Atlanta"), stringsAsFactors=FALSE)
