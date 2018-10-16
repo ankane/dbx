d0e98a22c880634250899f4ae9cababeee23fe52
Andrew <acekane1@gmail.com>
2018-10-15 18:22:30 -0700

    Fixed upsert tests

--- tests/testthat/helper-run-upsert-tests.R
+++ tests/testthat/helper-run-upsert-tests.R
@@ -2,34 +2,42 @@ runUpsertTests <- function(db, redshift=FALSE) {
   test_that("upsert works", {
     skip_if(isSQLite(db) || redshift || isSQLServer(db))
 
-    upsert_orders <- data.frame(id=c(3, 5), city=c("Boston", "Chicago"))
-    dbxUpsert(db, "orders", upsert_orders, where_cols=c("id"))
+    dbxDelete(db, "events")
 
-    res <- dbxSelect(db, "SELECT city FROM orders WHERE id IN (3, 5)")
-    expect_equal(res$city, c("Boston", "Chicago"))
+    events <- data.frame(id=c(1, 2), city=c("San Francisco", "Boston"), stringsAsFactors=FALSE)
+    dbxInsert(db, "events", events)
 
-    dbxDelete(db, "orders", data.frame(id=c(5)))
+    upsert_events <- data.frame(id=c(2, 3), city=c("Chicago", "New York"))
+    dbxUpsert(db, "events", upsert_events, where_cols=c("id"))
+
+    res <- dbxSelect(db, "SELECT city FROM events ORDER BY id")
+    expect_equal(res$city, c("San Francisco", "Chicago", "New York"))
   })
 
   test_that("upsert only where_cols works", {
     skip_if(isSQLite(db) || redshift || isSQLServer(db))
 
-    upsert_orders <- data.frame(id=c(3, 5))
-    dbxUpsert(db, "orders", upsert_orders, where_cols=c("id"))
+    dbxDelete(db, "events")
 
-    res <- dbxSelect(db, "SELECT city FROM orders WHERE id IN (3, 5)")
-    expect_equal(res$city, c("Boston", NA))
+    events <- data.frame(id=c(1, 2), city=c("San Francisco", "Boston"), stringsAsFactors=FALSE)
+    dbxInsert(db, "events", events)
 
-    dbxDelete(db, "orders", data.frame(id=c(5)))
+    upsert_events <- data.frame(id=c(2, 3))
+    dbxUpsert(db, "events", upsert_events, where_cols=c("id"))
+
+    res <- dbxSelect(db, "SELECT city FROM events ORDER BY id")
+    expect_equal(res$city, c("San Francisco", "Boston", NA))
   })
 
   test_that("upsert missing column raises error", {
-    update_orders <- data.frame(id=c(3), city=c("LA"))
-    expect_error(dbxUpsert(db, "orders", update_orders, where_cols=c("missing")), "where_cols not in records")
+    update_events <- data.frame(id=c(3), city=c("LA"))
+    expect_error(dbxUpsert(db, "events", update_events, where_cols=c("missing")), "where_cols not in records")
   })
 
   test_that("empty upsert works", {
     skip_if(!isPostgres(db) || redshift)
+
+    dbxDelete(db, "events")
 
     dbxUpsert(db, "events", data.frame(id = as.numeric(), active = as.logical()), where_cols=c("id"))
     expect(TRUE)
