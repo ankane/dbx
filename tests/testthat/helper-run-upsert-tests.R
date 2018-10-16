dcc8dcbad16b0d11ad735d2b3e46c4517306ae62
Andrew <acekane1@gmail.com>
2018-10-15 18:26:48 -0700

    Added upsert helper

--- tests/testthat/helper-run-upsert-tests.R
+++ tests/testthat/helper-run-upsert-tests.R
@@ -1,6 +1,10 @@
 runUpsertTests <- function(db, redshift=FALSE) {
+  upsertSupported <- function() {
+    !(isSQLite(db) || redshift || isSQLServer(db))
+  }
+
   test_that("upsert works", {
-    skip_if(isSQLite(db) || redshift || isSQLServer(db))
+    skip_if_not(upsertSupported())
 
     dbxDelete(db, "events")
 
@@ -15,7 +19,7 @@ runUpsertTests <- function(db, redshift=FALSE) {
   })
 
   test_that("upsert only where_cols works", {
-    skip_if(isSQLite(db) || redshift || isSQLServer(db))
+    skip_if_not(upsertSupported())
 
     dbxDelete(db, "events")
 
@@ -30,15 +34,13 @@ runUpsertTests <- function(db, redshift=FALSE) {
   })
 
   test_that("upsert missing column raises error", {
+    skip_if_not(upsertSupported())
+
     update_events <- data.frame(id=c(3), city=c("LA"))
     expect_error(dbxUpsert(db, "events", update_events, where_cols=c("missing")), "where_cols not in records")
   })
 
   test_that("empty upsert works", {
-    skip_if(!isPostgres(db) || redshift)
-
-    dbxDelete(db, "events")
-
     dbxUpsert(db, "events", data.frame(id = as.numeric(), active = as.logical()), where_cols=c("id"))
     expect(TRUE)
   })
