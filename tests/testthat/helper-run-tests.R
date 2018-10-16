3f41036d7573303eecbbaf1007fa1294c676ee88
Andrew <acekane1@gmail.com>
2018-10-15 18:28:34 -0700

    Removed commented code [skip ci]

--- tests/testthat/helper-run-tests.R
+++ tests/testthat/helper-run-tests.R
@@ -2,11 +2,6 @@
 Sys.setenv(TZ="America/Los_Angeles")
 
 runTests <- function(db, redshift=FALSE) {
-  # orders <- data.frame(id=c(1, 2), city=c("San Francisco", "Boston"), stringsAsFactors=FALSE)
-  # new_orders <- data.frame(id=c(3, 4), city=c("New York", "Atlanta"), stringsAsFactors=FALSE)
-
-  # dbxInsert(db, "orders", orders)
-
   runSelectTests(db)
   runInsertTests(db, redshift=redshift)
   runUpdateTests(db)
