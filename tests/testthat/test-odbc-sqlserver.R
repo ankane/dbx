63385f235b2e95bf51c908fa63038fc1f3848086
Andrew Kane <andrew@ankane.org>
2023-12-05 13:54:02 -0800

    Fixed tests for SQL Server [skip ci]

--- tests/testthat/test-odbc-sqlserver.R
+++ tests/testthat/test-odbc-sqlserver.R
@@ -2,9 +2,12 @@ context("odbc-sqlserver")
 
 skip("odbc")
 
-db <- dbxConnect(adapter=odbc::odbc(), driver="/usr/local/lib/libtdsodbc.so", database="dbx_test", server="localhost", port=1433, uid="SA", pwd="YourNewStrong!Passw0rd")
+db <- dbxConnect(adapter=odbc::odbc(), driver="/opt/homebrew/lib/libtdsodbc.so", database="dbx_test", server="localhost", port=1433, uid="SA", pwd="YourStrong!Passw0rd")
 
 dbxExecute(db, "DROP TABLE IF EXISTS events")
-dbxExecute(db, "CREATE TABLE events (id INT NOT NULL IDENTITY PRIMARY KEY, city VARCHAR(255), counter INT, bigcounter BIGINT, speed FLOAT, distance DECIMAL(5, 2), created_on DATE, updated_at DATETIME2(6), deleted_at DATETIME2(6), open_time TIME, active BIT, properties TEXT, propertiesb TEXT, image IMAGE)")
+dbxExecute(db, "DROP SEQUENCE IF EXISTS events_id_seq")
+
+dbxExecute(db, "CREATE SEQUENCE events_id_seq")
+dbxExecute(db, "CREATE TABLE events (id BIGINT NOT NULL PRIMARY KEY CONSTRAINT c DEFAULT NEXT VALUE FOR events_id_seq, city VARCHAR(255), counter INT, bigcounter BIGINT, speed FLOAT, distance DECIMAL(5, 2), created_on DATE, updated_at DATETIME2(6), deleted_at DATETIME2(6), open_time TIME, active BIT, properties TEXT, propertiesb TEXT, image IMAGE)")
 
 runTests(db)
