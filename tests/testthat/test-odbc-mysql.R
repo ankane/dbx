140740a330e91a0152f35bbe61a220accee61ee8
Andrew Kane <andrew@ankane.org>
2025-03-17 11:11:09 -0700

    Fixed test [skip ci]

--- tests/testthat/test-odbc-mysql.R
+++ tests/testthat/test-odbc-mysql.R
@@ -13,6 +13,6 @@ if (isMac()) {
 db <- dbxConnect(adapter=odbc::odbc(), driver=driver, database="dbx_test")
 
 dbxExecute(db, "DROP TABLE IF EXISTS events")
-dbxExecute(db, paste0("CREATE TABLE events (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, city VARCHAR(255), counter INT, bigcounter BIGINT, speed FLOAT, distance DECIMAL(20, 18), created_on DATE, updated_at DATETIME(6), deleted_at TIMESTAMP(6) NULL DEFAULT NULL, open_time TIME, active BOOLEAN, properties JSON, image BLOB)"))
+dbxExecute(db, paste0("CREATE TABLE events (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, city VARCHAR(255), counter INT, bigcounter BIGINT, speed FLOAT, distance DECIMAL(20, 18), created_on DATE, updated_at DATETIME(6), deleted_at TIMESTAMP(6) NULL DEFAULT NULL, open_time TIME(3), active BOOLEAN, properties JSON, image BLOB)"))
 
 runTests(db)
