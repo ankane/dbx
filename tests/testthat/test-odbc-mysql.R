7bd591e719fcdb38106c3d521faf302013e1fe85
Andrew Kane <andrew@ankane.org>
2025-03-17 00:08:22 -0700

    Simplified test code [skip ci]

--- tests/testthat/test-odbc-mysql.R
+++ tests/testthat/test-odbc-mysql.R
@@ -6,7 +6,6 @@ skip("odbc")
 db <- dbxConnect(adapter=odbc::odbc(), driver="/opt/homebrew/lib/mariadb/libmaodbc.dylib", database="dbx_test")
 
 dbxExecute(db, "DROP TABLE IF EXISTS events")
-json_type <- if (Sys.getenv("TRAVIS") == "") "JSON" else "TEXT"
-dbxExecute(db, paste0("CREATE TABLE events (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, city VARCHAR(255), counter INT, bigcounter BIGINT, speed FLOAT, distance DECIMAL(20, 18), created_on DATE, updated_at DATETIME(6), deleted_at TIMESTAMP(6) NULL DEFAULT NULL, open_time TIME, active BOOLEAN, properties ", json_type, ", image BLOB)"))
+dbxExecute(db, paste0("CREATE TABLE events (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, city VARCHAR(255), counter INT, bigcounter BIGINT, speed FLOAT, distance DECIMAL(20, 18), created_on DATE, updated_at DATETIME(6), deleted_at TIMESTAMP(6) NULL DEFAULT NULL, open_time TIME, active BOOLEAN, properties JSON, image BLOB)"))
 
 runTests(db)
